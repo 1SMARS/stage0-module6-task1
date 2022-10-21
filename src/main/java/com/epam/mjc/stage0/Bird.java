3b10f47cbc07a5ae8591f3abbf33c4bb51c74333
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-21 20:57:28 +0600

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+class Bird extends Animal {
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
 
+    @Override
+    public String getDescription() {
+        return "This animal is mostly " + color + " It has " + numberOfPaws + " paws and " + " no fur " + hasFur + " Moreover, it has " + numberOfPaws + " wings and can fly.";
+
+    }
 }
