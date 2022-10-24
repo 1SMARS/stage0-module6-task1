2b6b4b08ac077d49b165a309c5ee82efa11940b7
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-25 02:46:40 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -45,16 +45,16 @@ public class Animal {
     public String getDescription() {
         if (hasFur) {
             if (numberOfPaws > 1)
-                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and" + " a fur";
+                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and" + " a fur.";
             else
-                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and" + " a fur";
+                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and" + " a fur.";
 
         }
         else if (!hasFur) {
             if (numberOfPaws > 1)
-                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and" + " no fur";
+                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and" + " no fur.";
             else
-                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and" + " no fur";
+                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and" + " no fur.";
 
         }
         else 
