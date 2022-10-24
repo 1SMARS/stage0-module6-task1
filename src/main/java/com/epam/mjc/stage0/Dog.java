b439654fa8a689b69bb697b2e196676f7b50e8bd
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-25 02:40:26 +0600

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,6 +1,11 @@
 package com.epam.mjc.stage0;
 
 public class Dog extends Animal {
+  
+  public Dog() {
+        
+    }
+  
   public Dog(String color, int numberOfPaws, boolean hasFur) {
         super(color, numberOfPaws, hasFur);
     }
