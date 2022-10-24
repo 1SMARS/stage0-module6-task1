30369a95dfcc4b4c3836a1dba3eef27920725988
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-25 03:26:48 +0600

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -3,7 +3,9 @@ package com.epam.mjc.stage0;
 public class Dog extends Animal {
   
   public Dog() {
-        System.out.println("This animal is mostly brown. It has 4 paws and a fur.");
+        setColor("brown");
+        setHasFur(true);
+        setNumberOfPaws(4);
     }
   
   public Dog(String color, int numberOfPaws, boolean hasFur) {
